1d6a98e9c196fc8d893e2f0ed7f0d1d701d4daef
rudolfselinka <147338304+rudolfselinka@users.noreply.github.com>
2026-01-16 21:52:49 +0100

    send.php

--- Mini-Atlas-Rostlin/Mini-Atlas-Rostlin/php/send.php
+++ Mini-Atlas-Rostlin/Mini-Atlas-Rostlin/php/send.php
@@ -1,18 +1,30 @@
 <?php
+/**
+ * Zpracování kontaktního formuláře a odeslání e-mailu
+ */
+
+// Kontrola, zda byla data odeslána metodou POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    // Sanitizace vstupních dat proti XSS útokům pomocí htmlspecialchars
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
+    // Konfigurace příjemce a předmětu zprávy
     $to = "selinka.rudolf@gmail.com";
     $subject = "Nový dotaz z Mini Atlasu";
+    
+    // Sestavení těla e-mailu
     $body = "Od: $name\nE-mail: $email\n\nZpráva:\n$message";
 
+    // Odeslání e-mailu pomocí vestavěné funkce mail()
     if (mail($to, $subject, $body)) {
+        // Úspěšné odeslání – uživatel uvidí potvrzení
         echo "Zpráva byla úspěšně odeslána!";
     } else {
+        // Chybový stav v případě selhání poštovního serveru
         echo "Došlo k chybě při odesílání.";
     }
 }
-
 ?>
